2f4c065aab0581e9c7ea90a98cea0e27ab80a61b
Archishman Nag <nag.archishman@gmail.com>
2025-07-11 22:24:37 +0530

    Update LLM prompt

--- llm.py
+++ llm.py
@@ -150,6 +150,23 @@ User query:
     }
   }
 }
+
+The data for Sankey must be in the following format in accordance to Recharts:
+{
+    'nodes': [
+        { 'name': 'Visit' },
+        { 'name': 'Direct-Favourite' },
+        { 'name': 'Page-Click' },
+        { 'name': 'Detail-Favourite' },
+        { 'name': 'Lost' },
+    ],
+    'links': [
+        { 'source': 0, 'target': 1, 'value': 3728.3 },
+        { 'source': 0, 'target': 2, 'value': 354170 },
+        { 'source': 2, 'target': 3, 'value': 62429 },
+        { 'source': 2, 'target': 4, 'value': 291741 },
+    ],
+}
 """
     )
 
